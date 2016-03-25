c0820f7f8b3e58fa4a80b1dd5f9fa8d09a6d718e
CeciliaLiu <ziyue.liu7@gmail.com>
2016-03-25 11:27:58 -0500

    My first commit

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,11 +3,11 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
-        System.out.println(System.getProperty("java.version"));
+        return Double.parseDouble(System.getProperty("java.specification.version"));
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
-	    getVersion();
+	    System.out.println(getVersion());
     }
 }

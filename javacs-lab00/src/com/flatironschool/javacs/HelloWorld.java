a2fd5fc4c184e2bb7bdf1ea439986b8d78183255
liuziyue <ziyue.liu7@gmail.com>
2016-03-24 23:58:47 -0500

    Update HelloWorld.java

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,7 +3,7 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
-        System.out.println(System.getProperty("MyJava"));
+        System.out.println(System.getProperty("java.version"));
     }
 
     public static void main(String[] args) {
